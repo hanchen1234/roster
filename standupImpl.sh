905a4bcb72bf651e07d405a9bf7bb5ac6be3a825
hanchen <han.chen1@thoughtworks.com>
2025-04-14 10:44:43 +0800

    Merge remote-tracking branch 'origin/main'

--- standupImpl.sh
+++ standupImpl.sh
@@ -2,18 +2,31 @@
 
 WEBHOOK_URL="https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=90977e98-2843-4836-b669-f28d6f2b72c6"
 
-ROSTER=("Ying" "Wenbin" "Han" "Xiao" "Yu")
+people=("俞潇" "付昱" "陈晗" "付莹" "刘文宾")
 
-index=$(cat standup_index.txt)
-next_index=$((index + 1))
-next_index=$((next_index % ${#ROSTER[@]}))
+start_date="2025-04-14"
+today=$(date +%Y-%m-%d)
 
-weekday=$(date +%u)
-if [[ $weekday -le 5 ]]; then
-    person=${ROSTER[next_index]}
-    MESSAGE="{\"msgtype\": \"text\", \"text\": {\"content\": \"Congrats! standup host is：$person\"}}"
-    curl -X POST -H "Content-Type: application/json" -d "$MESSAGE" "$WEBHOOK_URL"
-    echo $next_index > standup_index.txt
-else
-    echo "weekend off"
+start_ts=$(date -d "$start_date" +%s)
+today_ts=$(date -d "$today" +%s)
+
+if [ "$start_ts" -gt "$today_ts" ]; then
+  echo "${people[0]}"
+  exit 0
 fi
+
+work_days=0
+
+current_date="$start_date"
+while [ "$(date -d "$current_date" +%s)" -le "$today_ts" ]; do
+  day_of_week=$(date -d "$current_date" +%u)
+  if [ "$day_of_week" -lt 6 ]; then
+    work_days=$((work_days + 1))
+  fi
+  current_date=$(date -d "$current_date +1 day" +%Y-%m-%d)
+done
+
+index=$((work_days % ${#people[@]}))
+person=${people[$index]}
+MESSAGE="{\"msgtype\": \"text\", \"text\": {\"content\": \"Congrats! standup host is：$person\"}}"
+curl -X POST -H "Content-Type: application/json" -d "$MESSAGE" "$WEBHOOK_URL"
